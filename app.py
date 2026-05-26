1a99c328dd419ec48056f4e99d6277ded1aaa228
arup-kumar <arupkumard31@gmail.com>
2026-05-26 11:31:46 +0530

    new lines updated

--- app.py
+++ app.py
@@ -6,3 +6,26 @@ print("Hello", name)
 print("Welcome to Python Programming")
 
 print=2000+3000+1000=6000
+
+
+
+
+-------git branch commands------------
+
+
+     git branch                  # show local branches
+git branch -a               # show all branches
+git checkout dev            # switch branch
+git checkout -b dev         # create + switch branch
+git switch dev              # switch branch (modern)
+git switch -c feature       # create + switch branch
+git merge dev               # merge dev into current branch
+git branch -d dev           # delete branch
+git branch -D dev           # force delete branch
+git push origin dev         # push branch to remote
+git pull origin main        # pull latest code
+git fetch                   # fetch remote updates
+git rebase main             # rebase current branch
+git branch -m old new       # rename branch
+git remote show origin      # check remote branches
+git push --set-upstream origin dev   # first push for new branch
